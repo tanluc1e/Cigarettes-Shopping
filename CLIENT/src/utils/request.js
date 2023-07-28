6f3d2cc9603bf573cff630952cf1c7269e3c0ac0
Luke <93022030+tanluc1e@users.noreply.github.com>
2023-07-28 12:04:46 +0700

    Update request.js

--- CLIENT/src/utils/request.js
+++ CLIENT/src/utils/request.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const request = axios.create({
   // 'http://localhost:8080/'
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://cuahangthuocla.onrender.com/api",
 });
 export default request;
